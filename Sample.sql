f769e4dbf29c77998d0d53c7dbeff2d998875bb4
fa1at <167025749+fa1at@users.noreply.github.com>
2024-10-21 11:08:11 +0200

    Update and rename Hackerrank to Sample.sql

---
+++ Sample.sql
@@ -0,0 +1,31 @@
+/*
+#1 Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. The STATION table is described as follows:
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+*/  
+SELECT (count(CITY)- count(distinct CITY)) 
+FROM STATION; 
+
+/*
+#2 Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+The STATION table is described as follows:
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+*/
