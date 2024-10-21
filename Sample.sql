f949c6ff430ef56447324b1e6334e00e401cb2c4
fa1at <167025749+fa1at@users.noreply.github.com>
2024-10-21 15:09:51 +0200

    Update Sample.sql

--- Sample.sql
+++ Sample.sql
@@ -26,8 +26,10 @@ The STATION table is described at the top.
 */
 
 /*
-#3 Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+#3 Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 The STATION table is described at the top.
+
+Zapytanie o listę nazw CITY ze STATION dla miast, które mają parzysty numer ID. Wydrukuj wyniki w dowolnej kolejności, ale wyklucz duplikaty z odpowiedzi. 
 */
 SELECT DISTINCT City
 FROM Station
@@ -58,5 +60,14 @@ SELECT DISTINCT City
 FROM Station
 WHERE LEFT(City, 1) IN ('A', 'E', 'I', 'O', 'U');
 
+/*
+#6 Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+Zapytanie o listę nazw CITY z STATION, które mają samogłoski (tj. a, e, i, o i u) jako pierwszy i ostatni znak. Wynik nie może zawierać duplikatów.
+*/
+SELECT DISTINCT City
+FROM Station
+WHERE LEFT(City, 1) IN ('A', 'E', 'I', 'O', 'U')
+AND RIGHT(City, 1) IN ('A', 'E', 'I', 'O', 'U');
 
 
